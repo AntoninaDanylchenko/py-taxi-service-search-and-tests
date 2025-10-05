30d519ea0e225ad0e39d25ba7bae6c2272448b2e
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-10-05 15:02:12 +0200

    Add some tests to driver

--- taxi/tests/test_driver_view.py
+++ taxi/tests/test_driver_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from taxi.forms import DriversSearchForm
 from taxi.models import Driver, Car, Manufacturer
 
 DRIVER_LIST_URL = reverse("taxi:driver-list")
@@ -15,15 +16,29 @@ class PublicDriversViewTests(TestCase):
 class PrivateDriversViewTests(TestCase):
     def setUp(self):
         self.user = Driver.objects.create_user(
-            username="driver1",
-            password="test123"
+            username="testuser",
+            password="test_123",
         )
         self.client.force_login(self.user)
+
+        self.driver1 = Driver.objects.create_user(
+            username="alice",
+            password="pass123",
+            license_number="ABC12345"
+        )
+        self.driver2 = Driver.objects.create_user(
+            username="bob",
+            password="pass456",
+            license_number="XYZ67890"
+        )
 
     def test_toggle_assign_to_car(self):
         car = Car.objects.create(
             model="X5",
-            manufacturer=Manufacturer.objects.create(name="BMW")
+            manufacturer=Manufacturer.objects.create(
+                name="BMW",
+                country="USA"
+            )
         )
         url = reverse("taxi:toggle-car-assign", args=[car.id])
         response = self.client.get(url)
@@ -32,3 +47,32 @@ class PrivateDriversViewTests(TestCase):
             reverse("taxi:car-detail", args=[car.id])
         )
         self.assertIn(car, self.user.cars.all())
+
+    def test_list_view_displays_drivers(self):
+        response = self.client.get(DRIVER_LIST_URL)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/driver_list.html")
+        self.assertIn(self.driver1, response.context["driver_list"])
+        self.assertIn(self.driver2, response.context["driver_list"])
+
+    def test_search_filters_queryset(self):
+        """
+        Search form should filter drivers by username.
+        """
+        response = self.client.get(DRIVER_LIST_URL, {"username": "ali"})
+
+        driver_list = response.context["driver_list"]
+
+        self.assertIn(self.driver1, driver_list)
+        self.assertNotIn(self.driver2, driver_list)
+
+    def test_context_contains_search_form(self):
+        """
+        The view context should include the DriversSearchForm.
+        """
+        response = self.client.get(DRIVER_LIST_URL)
+
+        form = response.context.get("search_form")
+        self.assertIsInstance(form, DriversSearchForm)
+        self.assertIn("username", form.fields)
